1fdec5c4da726d426683637865c97eb7953562ab
udit_tiwari <udit.tiwari@quantive.com>
2023-07-17 15:51:54 +0530

    july 17 useful operator

---
+++ Python/Statements/useful_operator.py
@@ -0,0 +1,46 @@
+index_count =0 
+
+for letter in 'abcde':
+    # print('At index {} the letter is {}'.format(index_count,letter))
+    index_count +=1
+    
+    
+#if we want to get words 
+index_count =0 
+word = 'abcde'
+for letter in word:
+    # print(word[index_count]) #we get index number now normally how we acces element in list we can do like that list[index_number]
+    index_count +=1
+    
+    
+#Instead of this we use enumerate
+word = 'abcde'
+for item in enumerate(word):
+    print(item)#Result will be tuple containg (index,value)
+    
+    
+word = 'abcde'
+for index,letter in enumerate(word):
+    print(index,letter)
+
+
+#ZIP 
+mylistone=[1,2,3]
+mylisttwo=['a','b','c']
+
+for item in zip(mylistone,mylisttwo):
+    print(item) #return tuples
+    
+#In keyword
+
+check = 2 in [1,2,3]
+print(check)
+check = 'x' in ['t','x','z']
+
+checkdic = 'mykey' in {'mykey':345}
+print(checkdic)
+
+cdic = {'mykey':345}
+keyvalue = 345 in cdic.values()
+if keyvalue:
+    print("Present")
