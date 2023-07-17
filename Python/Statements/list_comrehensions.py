cba4d2c8caa2d25662403b13e06975a2072e4497
udit_tiwari <udit.tiwari@quantive.com>
2023-07-17 22:40:01 +0530

    july 17 methods and fucntions

--- Python/Statements/list_comrehensions.py
+++ Python/Statements/list_comrehensions.py
@@ -11,3 +11,30 @@ print(mylist)
 
 newlist = [ letter for letter in mystring]
 print('new list',newlist)
+#cel to fahrenheit
+cel = [0,10,20,34.5]
+fah = [((9/5)*temp + 32) for temp in cel]
+print(fah)
+
+
+
+result = [ 'EVEN' if x%2==0 else 'ODD' for x in range(0,11)]
+print(result)
+
+
+
+#####################
+mynolist = []
+for x in [2,4,6]:
+    for y in [1,2,1000]:
+        mynolist.append(x*y)
+
+print(mynolist)
+
+#now using list comprehensions
+
+mylistyz = [x*y for y in [1,2,1000] for x in [2,4,6]]
+print('mylistyz',mylistyz)
+
+mylistxy = [x*y for x in [2,4,6] for y in [1,10,1000]]
+print(mylistxy)
