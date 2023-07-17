31c1ae700ceac4f90e46866f8b9695b6061d8b87
udit_tiwari <udit.tiwari@quantive.com>
2023-07-17 15:07:46 +0530

    july 17

--- Python/Statements/for_loop.py
+++ Python/Statements/for_loop.py
@@ -1,6 +1,6 @@
-#my_iterable = [1,2,3]
-#for item_name in my_iterable:
-    #print(item_name)
+# #my_iterable = [1,2,3]
+# #for item_name in my_iterable:
+#     #print(item_name)
     
     
 mylist = [1,2,3,4,5,6,7]
@@ -13,10 +13,44 @@ for num in mylist:
         
         
 #Sum the list
-
 list_sum = 0
 for num in mylist:
     list_sum += num
     #  print("inside loop",list_sum)
 
 print("Sum",list_sum)
+
+
+
+mytuplist = [(1,2),(3,4),(5,6),(7,8)]
+# print(len(mytuplist))
+
+# for item in mytuplist:
+#     print(item)
+
+# tuple Unpakcing
+for a,b in mytuplist:
+    print(a) # print first element from all tuple
+    # print(b)
+    
+    
+d = {"k1":1,"k2":2,"k3":3}
+
+for item in d:
+    print(item)
+
+#now we want item pairs key : value both
+for item in d.items():
+    print(item)
+    
+#only value
+for value in d.values():
+    print(value)
+    
+#i want key ,valkue in diffrent var (using tuple unpacking)
+
+for key,value in d.items():
+    print(key,value)
+    
+    
+    
--- Python/Statements/while_loop.py
+++ Python/Statements/while_loop.py
@@ -1,3 +1,10 @@
-
-
+#while some_boolean_condition:
+    #do something
+# else:
+    #do someting diffrent 
     
+    
+x = 0 
+while x<5:
+    print(f"The current vakue of x is {x}")
+    x+=1
